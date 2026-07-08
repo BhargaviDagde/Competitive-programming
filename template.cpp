6984768f0d963751f3e1028f75160f09f49cac72
BhargaviDagde <bhargavidagde@gmail.com>
2026-07-08 08:46:18 +0530

    added boilerplate

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // write your solution here
+
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t; // number of test cases; remove this line + the loop below if there's only one test case
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
